ae7a821904e605804917ab8375e105546099544e
jswan <jswannieman@gmail.com>
2023-07-18 13:01:39 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 
 const DetermineDistance = () => {
-  const [meters, setMeters] = useState(10);
+  const [meters, setMeters] = useState(0);
+  const [totalDistanceLeft, setTotalDistanceLeft] = useState(5000);
 
   const incrementMeters = () => {
     setMeters((prevState) => prevState + 10);
+    setTotalDistanceLeft(totalDistanceLeft - 10);
   };
 
   return (
     <div>
-      The old man has walked {meters} meters today
+      <h4>The old man has walked {meters} meters today</h4>
+      <h4>
+        His distance left to still walk for the week is {totalDistanceLeft}
+      </h4>
       <button onClick={incrementMeters}>Old man walk</button>
     </div>
   );
